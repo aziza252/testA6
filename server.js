38adabca8ded7b85c59fb4fe09937b5f79a2643c
Prince <proka-magar@myseneca.ca>
2025-04-12 22:27:01 -0400

    add

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ const jwtOptions = {
     secretOrKey: process.env.JWT_SECRET
 };
 
-// JWT Strategy
+console.log("Starting server.js...");
+console.log("JWT Secret from Environment:", process.env.JWT_SECRET);
+
 const jwtStrategy = new passportJWT.Strategy(jwtOptions, (jwt_payload, done) => {
     userService.getUserById(jwt_payload._id)
         .then(user => {
