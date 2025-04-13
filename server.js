62ed6118847577fc3069f8d7cb63b038e497b3ae
Prince <proka-magar@myseneca.ca>
2025-04-12 22:56:58 -0400

    debug

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const jwtOptions = {
     secretOrKey: process.env.JWT_SECRET
 };
 
-console.log("Starting server.js...");//
-console.log("JWT Secret from Environment:", process.env.JWT_SECRET);
+//console.log("Starting server.js...");//
+//console.log("JWT Secret from Environment:", process.env.JWT_SECRET);
 
 const jwtStrategy = new passportJWT.Strategy(jwtOptions, (jwt_payload, done) => {
     userService.getUserById(jwt_payload._id)
